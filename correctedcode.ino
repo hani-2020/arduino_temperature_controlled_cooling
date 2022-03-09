1709f0550e2a45b0282560ddd14fc6be4e4a1c19
hani-2020 <72330345+hani-2020@users.noreply.github.com>
2022-03-09 23:21:44 +0530

    Add files via upload

---
+++ correctedcode.ino
@@ -0,0 +1,20 @@
+int inpin=A0;
+int outpin=13;
+int val;
+int temp;
+int cond=25;
+void setup() {
+pinMode(inpin, INPUT);
+pinMode(13, OUTPUT);
+Serial.begin(9600);
+}
+
+void loop() {
+val=analogRead(inpin);
+temp=(val-500)/100;
+  if (temp>cond){
+    digitalWrite(outpin, HIGH);}
+  delay(5000);
+  digitalWrite(outpin, LOW);
+  Serial.println(temp);
+}
